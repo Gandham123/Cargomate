355344ba0abef267c1cd9c59e5ade16e9e41f5c5
Satya Bhaskar Gandham <gandhamnihal1@gmail.com>
2024-11-28 07:53:55 +0530

    Update index.js

--- my-app/src/Components/firebase/index.js
+++ my-app/src/Components/firebase/index.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbK2hxUaqsswSjaCy29v_XdHamYBh6ceM",
-  authDomain: "majorproject-b42fe.firebaseapp.com",
-  projectId: "majorproject-b42fe",
-  storageBucket: "majorproject-b42fe.appspot.com",
-  messagingSenderId: "761046908150",
-  appId: "1:761046908150:web:3515e04e5f045b1f7040d7"
+  apiKey:"****************",
+  authDomain: "******************",
+  projectId: "*********************",
+  storageBucket: "********************",
+  messagingSenderId: "********************",
+  appId: "***************"
 };
 
 // Initialize Firebase
